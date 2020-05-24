1de96cd0b2ee810ac20a84d42d1c99733f5d8ba4
emiliano.arregui <emiliano.arregui@gmail.com>
2020-05-24 13:43:01 -0300

    corrección versiones

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
     $("#diez,#veinte,#cincuenta,#cien,#docientos,#quinientos,#mil").keyup(function subtotal (){
 
@@ -16,10 +15,5 @@ $(document).ready(function(){
         })
         var total = lista.reduce((a, b) => a + b, 0);
         $("#total").val(total)
-=======
-$(document).onload(function(){
-    $("#diez,#veinte,#cincuenta,#cien,#docientos,#quinientos,#mil").onchange(function subtotal (){
-        console.log(self)
->>>>>>> 2d74b17ee87c2c050ca90903bec3784e9b1ea65b
     })
 })
